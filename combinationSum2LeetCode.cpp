eabd6e85f03a66f63091b596c2d3d977df48e9f8
soumak09 <soumak2016@gmail.com>
2022-01-11 07:44:02 +0530

    Add files via upload

---
+++ combinationSum2LeetCode.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution {
+public:
+    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> ds){
+            if(target == 0){
+                ans.push_back(ds);
+                return;
+        }
+        for(int i=ind; i<arr.size(); i++){
+            if(i>ind && arr[i]==arr[i-1]){
+                continue;
+            }
+            if(arr[i]>target){
+                break;
+            }
+            ds.push_back(arr[i]);
+            findCombination(i+1, target-arr[i], arr, ans, ds);
+            ds.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findCombination(0, target, candidates, ans, ds);
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> A;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            A.push_back(x);
+        }   
+        int sum;
+        cin>>sum;
+        Solution ob;
+        vector<vector<int>> result = ob.combinationSum(A, sum);
+   		if(result.size()==0){
+   			cout<<"Empty";
+   		}
+        for(int i=0;i<result.size();i++){
+            cout<<'(';
+            for(int j=0;j<result[i].size();j++){
+                cout<<result[i][j];
+                if(j<result[i].size()-1)
+                    cout<<" ";
+            }
+            cout<<")";
+        }
+        cout<<"\n";
+    }
+}	  // } Driver Code Ends
